150f570b9ea76bcb0cc00198f3f76cbaa68033c9
WT <wilsonwttran@gmail.com>
2023-03-19 00:57:40 -0700

    color changes and button rounding

--- src/components/RatingCard.tsx
+++ src/components/RatingCard.tsx
@@ -1,12 +1,14 @@
 import React from "react"
 import styles from "./RatingCard.module.css"
+import star from "../assets/icon-star.svg"
 
 export default function RatingCard() {
   return (
     <div className={styles.card}>
+      <img className={styles.star} src={star} alt="orange star icon" />
       <h1 className={styles.title}>How did we do?</h1>
       <p className={styles.description}>Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
-      <div>{[1, 2, 3, 4, 5].map((rating) => <button>{rating}</button>)}</div>
+      <div className={styles.container}>{[1, 2, 3, 4, 5].map((rating) => <button className={styles.ratings}>{rating}</button>)}</div>
       <button className={styles.submit}>SUBMIT</button>
     </div>
     
